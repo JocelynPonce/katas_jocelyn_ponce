5dd3bbb7f8b7b4d92c6d51b1f06c674fc85edce7
JocelynPonce <99515496+JocelynPonce@users.noreply.github.com>
2022-02-20 21:56:37 -0600

    Delete asteroide = 21.py

--- asteroide = 21.py
+++
@@ -1,7 +0,0 @@
-asteroide = 21
-if asteroide > 20:
-    print('look up! ¡Hay una luz mágica en el cierlo')
-elif asteroide == 20:
-    print('Look up! ¡Hay una luz mágica en el cielo!')
-else:
-    print('Nada que ver aquí')
